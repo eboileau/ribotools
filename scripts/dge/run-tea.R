d695199c94fd3d58292751c6a6cfb35a53d7e2bf
eboileau <boileau@uni-heidelberg.de>
2023-07-25 16:41:40 +0200

    UPD tea scripts

--- scripts/dge/run-tea.R
+++ scripts/dge/run-tea.R
@@ -1,41 +1,23 @@
-#!/biosw/R/3.5.1/bin/Rscript
-# #!/usr/bin/Rscript
+#! /usr/bin/env Rscript
 
-## Usage: ./run-degRibo-from-htseq.R [mm/rn/hs] [config] [num] [denom] [dirloc]
+# Usage: ./pre-sample-table.R [-sample SAMPLE] [-contrast CONTRAST] [-output OUTPUT] <-method LRT/deltaTE> <--batch> <--filter>
+# 1: [-sample SAMPLE]      Sample table (CSV) - or output from prep-sample-table.R
+# 2. [-contrast CONTRAST]  CSV file with contrasts to test on each line in 2 columns w/o header - condition,reference
+# 3. [-output OUTPUT]      Output directory
+# 4. <-method LRT/deltaTE> Method to use (default LRT)
+# 5. <--batch>             If present, uses "batch" from sample table
+# 6. <--filter>            If present, filter genes
 
-## mm = mouse, rn = rat, hs = human
-## config: same configuration file used when calling run-htseq-workflow
-## num:    condition level to compare, e.g. treatment
-## denom:  reference condition level, e.g. control
-## dirloc: output directory (full path), also used as tmp/
 
-## NOTE: num, denom must match the names used by run-htseq-workflow
-##       in constructing the file names, given by ribo_ and rnaseq_sample_name_map
+library(DESeq2)
+library(IHW)
+library(ashr)
 
-##       We assume that gene ids are ensembl ids ** TODO generalise, pass keytype as args
+library(dplyr)
+library(tibble)
+library(purrr)
 
-# ---------------------------------------------------------
-
-## Ribo- and RNA-seq are both filtered independently and ONLY genes where
-## both have non-zero values for ALL replicates are kept.
-## Joint normalisation is performed within DESeq, see below for separate normalisation.
-## ** TODO test separate normalisation, relax filter (non-zero ALL), to allow
-##         Ribo- and/or RNA-"specific" genes
-
-## Independent filtering using row median (mean is default)
-
-## Ribo- and RNA-seq LFC are determined using pairwise comparisons (Wald test).
-## We set a LFC threshold for the results, however we cannot do this for the shrunken FC
-## values. For the latter, we filter the results using the "un-shrunken p-values" and LFC.
-
-## Ratio of ratios (interaction term) is used to determined if genes are regulated differently
-## in ribo vs. rna: (ribo/rna)_Trt / (ribo/rna)_Ctrl = (ribo_Trt/ribo_Ctrl) / (rna_Trt/rna_Ctrl).
-## In the latter, we use LRT to compare the full model to the reduced model to identify significant genes.
-## The p-values are determined by the difference in deviance between the full and reduced model and
-## not the LFC. Thus for this term, we only filter based on the "un-shrunken p-values".
-## There are no LFC threshold set.
-
-## Shrinking done using "ashr".
+library(openxlsx)
 
 # ---------------------------------------------------------
 
@@ -46,29 +28,7 @@ altHypothesis.set <- "greaterAbs"
 alpha.set <- 0.05
 
 # ---------------------------------------------------------
-
-library(DESeq2)
-library(IHW)
-library(ashr)
-
-library("Glimma")
-library("genefilter")
-
-library("org.Mm.eg.db")
-library("org.Hs.eg.db")
-library("org.Rn.eg.db")
-
-library(yaml)
-
-library(dplyr)
-library(tibble)
-library(purrr)
-
-library(openxlsx)
-
-# ---------------------------------------------------------
 ## Functions
-
 
 run_analysis_fit_sub_lrt <- function(contrast, coldata, dds, model) {
 
@@ -109,7 +69,7 @@ run_analysis_fit_sub_lrt <- function(contrast, coldata, dds, model) {
 
     ## effect of condition on ribo counts: condition_Trt_vs_Ctrl + assayribo.conditionTrt
     res.ribo <- results(dds.sub,
-                        contrast=list(c(resultsNames(dds.sub)[3],resultsNames(dds.sub)[4])),
+                        contrast=list(c(resultsNames(dds.sub)[3], resultsNames(dds.sub)[4])),
                         lfcThreshold=lfcThreshold.set,
                         altHypothesis=altHypothesis.set,
                         alpha=alpha.set,
@@ -240,7 +200,7 @@ run_analysis_fit_sub_deltaTE <- function(contrast, coldata, dds, model) {
                         filterFun=ihw,
                         filter=rowMedians(counts(dds.ribo)))
     res.ribo$padj[is.na(res.ribo$padj)] <- 1
-    res.ribo <- lfcShrink(dds.ribo, coef=2, type="apeglm", res=res.ribo)
+    #res.ribo <- lfcShrink(dds.ribo, coef=2, type="apeglm", res=res.ribo)
 
     dds.rna <- DESeqDataSetFromMatrix(countData=cts.rna,
                                        colData=coldata.rna,
@@ -255,7 +215,7 @@ run_analysis_fit_sub_deltaTE <- function(contrast, coldata, dds, model) {
                        filter=rowMedians(counts(dds.rna)), # comment for default independent filtering (mean)
                        test="Wald")
     res.rna$padj[is.na(res.rna$padj)] <- 1
-    res.rna <- lfcShrink(dds.rna, coef=2, type="apeglm", res=res.rna)
+    #res.rna <- lfcShrink(dds.rna, coef=2, type="apeglm", res=res.rna)
 
     all.res <- list("dte"=res.inter,
                     "rna"=res.rna,
@@ -360,7 +320,7 @@ write_results <- function(dds, all.res, num, denom) {
     writeDataTable(wb, sheet=1, x=res)
 
     filen <- paste0("contrast_", num, "_vs_", denom, sep="")
-    dirloc.sub <- file.path(dirloc.out, filen, fsep=.Platform$file.sep)
+    dirloc.sub <- file.path(args$output, filen, fsep=.Platform$file.sep)
     created <- ifelse(!dir.exists(dirloc.sub), dir.create(dirloc.sub, recursive=TRUE), FALSE)
     if (!created) { print(paste("Using existing directory ", dirloc.sub, " ...", sep="")) }
     filen <- paste0(filen, ".xlsx", sep="")
@@ -371,13 +331,13 @@ write_results <- function(dds, all.res, num, denom) {
 }
 
 
-read_symbols <- function(sampleTable, dirloc) {
+read_symbols <- function(samples, dirloc) {
     # read gene symbols from HTSeq output tables
-    l <- lapply( as.character( sampleTable[,2] ), function(fn) read.table( file.path( dirloc, fn ), fill=TRUE ))
+    l <- lapply( as.character( samples[,2] ), function(fn) read.table( file.path( dirloc, fn ), fill=TRUE ))
     if( ! all( sapply( l, function(a) all( head(a$V2, -5) == head(l[[1]]$V2, -5) ) ) ) )
         stop( "Gene names (second column) differ between files." )
     tbl <- sapply( l, function(a) a[,2] )
-    colnames(tbl) <- sampleTable[,1]
+    colnames(tbl) <- samples[,1]
     rownames(tbl) <- l[[1]]$V1
     oldSpecialNames <- c("no_feature","ambiguous","too_low_aQual","not_aligned","alignment_not_unique")
     specialRows <- (substr(rownames(tbl),1,1) == "_") | rownames(tbl) %in% oldSpecialNames
@@ -399,7 +359,7 @@ get_reg_layers <- function(res, dirloc) {
     write.table(exclusive[,1:2], file.path(dirloc, "exclusive.txt", fsep=.Platform$file.sep), quote=F, sep="\t", col.names = F, row.names = F)
 
     # direction of change is taken into account
-    both < which(res$padj.dte < alpha.set & res$padj.ribo < alpha.set & res$padj.rna < alpha.set)
+    both <- which(res$padj.dte < alpha.set & res$padj.ribo < alpha.set & res$padj.rna < alpha.set)
     # Translationally intensified genes have a significant change in TE that acts with the effect of transcription
     intensified <- res[both[which(res[both,]$log2FC.dte*res[both,]$log2FC.rna > 0)],]
     write.table(intensified[,1:2], file.path(dirloc, "intensified.txt", fsep=.Platform$file.sep), quote=F, sep="\t", col.names = F, row.names = F)
@@ -415,147 +375,89 @@ get_reg_layers <- function(res, dirloc) {
     plot(y=res$log2FC.ribo, x=res$log2FC.rna, xlab="RNA-seq log2FC",ylab = "Ribo-seq log2FC", asp=1, pch=16, col=rgb(128/255,128/255,128/255,0.1), ylim=c(-max_val,max_val), xlim=c(-max_val,max_val), cex=0.4)
     abline(a=0, b=1, col="gray")
     abline(h=0, v=0, col="gray")
-    points(y=res[res$gene %in% forwarded$gene,]$log2FC.ribo, x=res[res$gene %in% forwarded$gene,]$log2FC.rna, pch=16, col=rgb(0,0,1,1))
-    points(y=res[res$gene %in% exclusive$gene,]$log2FC.ribo, x=res[res$gene %in% exclusive$gene,]$log2FC.rna, pch=16, col=rgb(0,1,0,1))
-    points(y=res[res$gene %in% intensified$gene,]$log2FC.ribo, x=res[res$gene %in% intensified$gene,]$log2FC.rna, pch=16, col=rgb(1,0,0,1))
-    points(y=res[res$gene %in% buffered$gene,]$log2FC.ribo, x=res[res$gene %in% buffered$gene,]$log2FC.rna, pch=16, col=rgb(1,0,0,1))
+    points(y=res[res$gene %in% forwarded$gene,]$log2FC.ribo, x=res[res$gene %in% forwarded$gene,]$log2FC.rna, pch=21, col=rgb(0,0,1,1), cex=0.6)
+    points(y=res[res$gene %in% exclusive$gene,]$log2FC.ribo, x=res[res$gene %in% exclusive$gene,]$log2FC.rna, pch=21, col=rgb(0,1,0,1), cex=0.6)
+    points(y=res[res$gene %in% intensified$gene,]$log2FC.ribo, x=res[res$gene %in% intensified$gene,]$log2FC.rna, pch=21, col=rgb(1,0,0,1), cex=0.6)
+    points(y=res[res$gene %in% buffered$gene,]$log2FC.ribo, x=res[res$gene %in% buffered$gene,]$log2FC.rna, pch=22, col=rgb(1,0,0,1), cex=0.6)
+    legend(x="topleft",
+           legend=c("Forwarded", "Exclusive (DTEG)", "Intensified", "Buffered"),
+           col=c(rgb(0,0,1,1), rgb(0,1,0,1), rgb(1,0,0,1), rgb(1,0,0,1)), lwd=1, lty=c(0,0),
+           pch=c(21,21,21,22), bty='n')
     dev.off()
 }
 
 # ---------------------------------------------------------
 ## Call
 
-# defaults
-base.loc <- "count-tables"
-
 # arguments
-args <- commandArgs(trailingOnly=TRUE)
-if (length(args)<4) { stop("./run-tea.R [YAML] [CONTRASTS] [OUTPUT] [METHOD] <BATCH> <FILTER>\n", call.=FALSE) }
-# config file
-params <- yaml::read_yaml(args[1])
-# contrasts file
-contrasts <- read.table(args[2], header=T)
+defaults <- list(method="LRT", batch=FALSE, filter=FALSE)
+args <- R.utils::commandArgs(trailingOnly=TRUE, asValues=TRUE, defaults=defaults)
+if (length(args)<3) {
+    stop("./run-tea.R [-sample SAMPLE] [-contrast CONTRAST] [-output OUTPUT] <-method LRT/deltaTE> <--batch> <--filter>\n",
+         call.=FALSE)
+}
+# sample table file
+samples <- read.delim(args$sample, sep=",")
+# contrast file
+contrasts <- read.delim(args$contrast, sep=",", header=FALSE)
 # output directory
-dirloc.out <- args[3]
-created <- ifelse(!dir.exists(dirloc.out), dir.create(dirloc.out, recursive=TRUE), FALSE)
-if (!created) { print(paste("Using existing directory ", dirloc.out, " ...", sep="")) }
-# method 0=LRT, 1=deltaTE
+created <- ifelse(!dir.exists(args$output), dir.create(args$output, recursive=TRUE), FALSE)
+if (!created) { print(paste0("Using existing directory ", args$output, "...")) }
+# method
 method <- "LRT"
-if (as.integer(args[4])==1) {
-    method <- "deltaTE"
-}
-print(paste("Using the following method ", method, " ...", sep=""))
-# batch effect
-batch <- NULL
-if (length(args)>4) {
-    batch <- read.table(args[5], header=T)
-    colnames(batch) <- c("sampleName", "batch")
-}
-# filter 0 counts 0=No 1=Yes
-filter <- FALSE
-if (length(args)>5 & as.integer(args[6])==1) {
-    filter <- TRUE
-}
-
+if (args$method=="deltaTE") { method <- "deltaTE" }
+print(paste0("Using the following method ", method, "..."))
 
 # write tmp files to
 tmp.loc <- uuid::UUIDgenerate()
-dirloc.tmp <- file.path(dirloc.out, tmp.loc, fsep=.Platform$file.sep)
+dirloc.tmp <- file.path(args$output, tmp.loc, fsep=.Platform$file.sep)
 dir.create(dirloc.tmp)
 
-# first use separate locations, this is easier, then
-# link all files to base.loc, remove afterwards.
+# wrangle
+samples <- as.data.frame(samples) # need dataframe for DESEq
+samples$condition <- factor(samples$condition)
+samples$assay <- factor(samples$assay)
 
-dirloc.ribo <- params$riboseq_data
-dirloc.ribo <- file.path(dirloc.ribo, base.loc, fsep=.Platform$file.sep)
-ribo.files <- list.files(dirloc.ribo)
-
-dirloc.rna <- params$rnaseq_data
-dirloc.rna <- file.path(dirloc.rna, base.loc, fsep=.Platform$file.sep)
-rna.files <- list.files(dirloc.rna)
-
-## construct sample table
-# rna
-rna.table <- params$rnaseq_sample_name_map %>%
-    data.frame() %>% t %>% data.frame(stringsAsFactors=FALSE) %>%
-    dplyr::rename(sampleName = ".")
-
-rna.table <- rna.table %>%
-    rowwise() %>% mutate(fileName=rna.files[grep(sampleName, rna.files)])
-
-rna.table$assay <- 'rna'
-rna.table$condition <- NA
-conditions <- unique(as.vector(as.matrix(contrasts)))
-used <- lapply(conditions, function(c) {rna.table$condition[grep(c, rna.table$sampleName, fixed=TRUE)] <<- c})
-
-# ribo
-ribo.table <- params$riboseq_sample_name_map %>%
-    data.frame() %>% t %>% data.frame(stringsAsFactors=FALSE) %>%
-    dplyr::rename(sampleName = ".")
-
-ribo.table <- ribo.table %>%
-    rowwise() %>% mutate(fileName=ribo.files[grep(sampleName, ribo.files)])
-
-ribo.table$assay <- 'ribo'
-ribo.table$condition <- NA
-used <- lapply(conditions, function(c) {ribo.table$condition[grep(c, ribo.table$sampleName, fixed=TRUE)] <<- c})
-
-sampleTable <- bind_rows(ribo.table, rna.table)
-sampleTable <- as.data.frame(sampleTable) # need dataframe for DESEq
-sampleTable$condition <- factor(sampleTable$condition)
-sampleTable$assay <- factor(sampleTable$assay)
-
-if (!is.null(batch)) {
-    sampleTable <- cbind(sampleTable, batch[match(sampleTable$sampleName, batch$sampleName),]$batch)
-    colnames(sampleTable)[ncol(sampleTable)] <- 'batch'
+# check if we use batch
+if (args$batch) {
+    if (nlevels(sampleTable$batch)<2) {
+        args$batch <- FALSE
+        print("Ignoring --batch! Use 'batch' as column header, and at least 2 levels!")
+    } else {
+        samples$batch <- factor(samples$batch)
+    }
 }
-
-# write for reference
-write.csv(sampleTable, file=file.path(dirloc.out, "sampleTable.csv", fsep=.Platform$file.sep))
-
-# no filtering - should be the same as above
-ribo.files <- sampleTable$fileName[sampleTable$assay=="ribo"]
-rna.files <- sampleTable$fileName[sampleTable$assay=="rna"]
 
 # link all files for DESeq
-for (f in ribo.files) {
-    from <- file.path(dirloc.ribo, f, fsep=.Platform$file.sep)
-    to <- file.path(dirloc.tmp, f, fsep=.Platform$file.sep)
+for (from in samples$fileName) {
+    to <- file.path(dirloc.tmp, basename(from), fsep=.Platform$file.sep)
     file.symlink(from, to)
 }
 
-for (f in rna.files) {
-    from <- file.path(dirloc.rna, f, fsep=.Platform$file.sep)
-    to <- file.path(dirloc.tmp, f, fsep=.Platform$file.sep)
-    file.symlink(from, to)
-}
-
-## create DESEq data object
+# create DESEq data object
+samples$fileName <- basename(samples$fileName)
 model <- ~assay+condition+assay:condition
 if (method == "LRT") {
   model.reduced <- ~assay+condition
 } else if (method == "deltaTE") {
     model.reduced <- ~condition
-    if (!is.null(batch)) { model.reduced <- ~batch+condition }
+    if (args$batch) { model.reduced <- ~batch+condition }
 }
-ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable,
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(samples,
                                        directory=dirloc.tmp,
                                        design=model)
 
-stopifnot(all(sampleTable$sampleName == colnames(ddsHTSeq)))
+stopifnot(all(samples$sampleName == colnames(ddsHTSeq)))
 
 # always rna as reference level for assay
 ddsHTSeq$assay <- relevel(ddsHTSeq$assay, ref="rna")
 
-## annotation
+# annotation
 # the order should be the same...
-id.mapping <- read_symbols(sampleTable, dirloc.tmp)
+id.mapping <- read_symbols(samples, dirloc.tmp)
 
 # remove files
-for (f in list.files(dirloc.tmp)) {
-    to <- file.path(dirloc.tmp, f, fsep=.Platform$file.sep)
-    file.remove(to)
-}
+unlink(dirloc.tmp, recursive=TRUE)
 
 # separate normalisation ???
 # sf <- numeric(ncols(ddsHTSeq))
@@ -565,9 +467,9 @@ for (f in list.files(dirloc.tmp)) {
 # sf[idx.rna] <- estimateSizeFactorsFromMatrix(counts(ddsHTSeq)[,idx.rna])
 # sizeFactors(ddsHTSeq) <- sf
 
-## filter
+# filter
 
-if (filter) {
+if (args$filter) {
     print("Removing genes with 0 counts in each assay separately, using the intersection...")
 
     ddsHTSeq.cts <- counts(ddsHTSeq)
@@ -580,11 +482,12 @@ if (filter) {
 
     idx <- intersect(idx.ribo, idx.rna)
     ddsHTSeq <- ddsHTSeq[idx,]
+    id.mapping <- id.mapping[idx]
 }
 
 ## fit contrasts
 if (method == "LRT") {
-    apply(contrasts, 1, run_analysis_fit_sub_lrt, coldata=sampleTable, dds=ddsHTSeq, model=model.reduced)
+    apply(contrasts, 1, run_analysis_fit_sub_lrt, coldata=samples, dds=ddsHTSeq, model=model.reduced)
 } else if (method == "deltaTE") {
-    apply(contrasts, 1, run_analysis_fit_sub_deltaTE, coldata=sampleTable, dds=ddsHTSeq, model=model.reduced)
+    apply(contrasts, 1, run_analysis_fit_sub_deltaTE, coldata=samples, dds=ddsHTSeq, model=model.reduced)
 }
